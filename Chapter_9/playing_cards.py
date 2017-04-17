d5c872fd4e69ca0b07c7e9e3fd9b5c06d1a84d34
BlackJin1 <m.e.g.i.k@mail.ru>
2017-04-17 12:44:01 +0300

    Часть 9, игральные карты

--- Chapter_9/playing_cards.py
+++ Chapter_9/playing_cards.py
@@ -11,3 +11,67 @@ class Card(object):
     def __str__(self):
         rep = self.rank+self.suit
         return rep
+
+
+class Hand(object):
+    """ Рука: набор карт на руках одного человека """
+    def __init__(self):
+        self.cards = []
+
+    def __str__(self):
+        if self.cards:
+            rep = ""
+            for card in self.cards:
+                rep += str(card) + " "
+        else:
+            rep = "<пусто>"
+        return rep
+
+    def clear(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def give(self, card, other_hand):
+        self.cards.remove(card)
+        other_hand.add(card)
+
+# Основная часть программы
+card1 = Card(rank="A", suit="♠")
+print("Вывожу на экран данную карту")
+print(card1)
+card2 = Card(rank="2", suit="♠")
+card3 = Card(rank="3", suit="♠")
+card4 = Card(rank="4", suit="♠")
+card5 = Card(rank="5", suit="♠")
+print("Вывожу еще 4 карты")
+print(card2)
+print(card3)
+print(card4)
+print(card5)
+
+my_hand = Hand()
+print("\nПечатаю карты, которые у мменя на руке до раздачи.")
+print(my_hand)
+my_hand.add(card1)
+my_hand.add(card2)
+my_hand.add(card3)
+my_hand.add(card4)
+my_hand.add(card5)
+print("\nПечатаю пять карт, которые появились у меня в руках.")
+print(my_hand)
+
+you_hand = Hand()
+my_hand.give(card1, you_hand)
+my_hand.give(card2, you_hand)
+print("\nПервые две из моих карт я передал вам.")
+print("Теперь у вас на руках:")
+print(you_hand)
+print("А у меня на руках:")
+print(my_hand)
+
+my_hand.clear()
+print("\nУ меня на руках, после того как я сбросил все карты:")
+print(my_hand)
+input("\n\nНажмите Enter, чтобы выйти.")
